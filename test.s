4c95402aa79844c8ea8c1109587282fa542e30dd
Lucasmsai <7lucas.msai7@gmail.com>
2024-03-19 16:32:04 +0100

    Lacamarche

---
+++ test.s
@@ -0,0 +1,5 @@
+			# This code was produced by the CERI Compiler
+	.text		# The following lines contain the program
+	.globl main	# The main function must be visible from outside
+main:			# The main function body :
+	movq %rsp, %rbp	# Save the position of the stack's top
